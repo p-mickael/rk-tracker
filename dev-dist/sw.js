8f65ba996b5a9dbfff02807ee37c6493a7f6b5fe
p.mickael <p.mickael@gmail.com>
2023-10-27 23:44:01 +0200

    Working state

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-ab7aa862'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.bs7ickq0nfg"
+    "revision": "0.oudpkn8sfuo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {useState} from "react";
+import GaugeChart from "react-gauge-chart";
+
+const numberOfTrackingIntensity = 5
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [numberOfTokens, setNumberOfTokens] = useState(0)
+    const [rkThreshold, setRkThreshold] = useState(4)
+    const maxNumberOfTokens = numberOfTrackingIntensity * rkThreshold
+    const percent = numberOfTokens / maxNumberOfTokens
+    const trackingIntensity = Math.ceil(numberOfTokens / rkThreshold) || 1
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    function removeToken() {
+        if (numberOfTokens > 0) {
+            setNumberOfTokens(numberOfTokens - 1)
+        }
+    }
+
+    function addToken() {
+        if (numberOfTokens < maxNumberOfTokens) {
+            setNumberOfTokens(numberOfTokens + 1)
+        }
+    }
+
+    return (
+        <main className={"flex flex-col gap-3"}>
+            <section className={"grid grid-cols-2 grid-flow-row"}>
+                <div className={"border p-2"}>Intensité de la traque</div>
+                <div className={"border p-2"}>{trackingIntensity}</div>
+                <div className={"border p-2"}>Intensité de la traque</div>
+                <div className={"border p-2"}>{trackingIntensity}</div>
+            </section>
+            <section>
+                <h2>Number of tokens</h2>
+                <input
+                    type="number"
+                    value={rkThreshold}
+                    onChange={(e) => setRkThreshold(Number(e.target.value))}/>
+            </section>
+            <section>
+                <GaugeChart
+                    id="rk-gauge"
+                    nrOfLevels={numberOfTrackingIntensity}
+                    arcWidth={0.3}
+                    percent={percent}
+                    needleColor={"#000000"}
+                    hideText={true}
+                    animDelay={0}
+                />
+            </section>
+            <section className={"flex justify-center gap-3"}>
+                <button onClick={removeToken}>-</button>
+                <button onClick={addToken}>+</button>
+            </section>
+        </main>
+    )
 }
 
 export default App
