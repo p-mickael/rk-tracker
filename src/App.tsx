1ecf719fc31c1e2ca66638550bdbb376f18a038d
p.mickael <p.mickael@gmail.com>
2023-10-28 00:49:41 +0200

    Reset

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css'
-import {useState} from "react";
+import {useMemo, useState} from "react";
 import GaugeChart from "react-gauge-chart";
 
 const numberOfTrackingIntensity = 5
@@ -9,7 +9,11 @@ function App() {
     const [rkThreshold, setRkThreshold] = useState(4)
     const maxNumberOfTokens = numberOfTrackingIntensity * rkThreshold
     const percent = numberOfTokens / maxNumberOfTokens
-    const trackingIntensity = Math.ceil(numberOfTokens / rkThreshold) || 1
+    const trackingIntensity = useMemo(() => {
+        const intensity = Math.ceil((numberOfTokens + 1) / rkThreshold)
+        return intensity > numberOfTrackingIntensity ? numberOfTrackingIntensity : intensity
+    }, [numberOfTokens, rkThreshold, numberOfTrackingIntensity]);
+    const currentIntensityTokenAmount = numberOfTokens % rkThreshold
 
     function removeToken() {
         if (numberOfTokens > 0) {
@@ -23,20 +27,22 @@ function App() {
         }
     }
 
+    function resetToken() {
+        setNumberOfTokens(0)
+    }
+
     return (
-        <main className={"flex flex-col gap-3"}>
-            <section className={"grid grid-cols-2 grid-flow-row"}>
-                <div className={"border p-2"}>Intensité de la traque</div>
-                <div className={"border p-2"}>{trackingIntensity}</div>
-                <div className={"border p-2"}>Intensité de la traque</div>
-                <div className={"border p-2"}>{trackingIntensity}</div>
-            </section>
-            <section>
-                <h2>Number of tokens</h2>
-                <input
-                    type="number"
-                    value={rkThreshold}
-                    onChange={(e) => setRkThreshold(Number(e.target.value))}/>
+        <main className={"flex flex-col gap-4"}>
+            <section className={"grid grid-cols-2 grid-flow-row gap-1"}>
+                <div className={"border p-2 align-middle"}>Seuil RK</div>
+                <div className={"border p-2"}>
+                    <input
+                        className={"w-14 text-center rounded-md border p-1"}
+                        type="number"
+                        value={rkThreshold}
+                        onChange={(e) => setRkThreshold(Number(e.target.value))}/></div>
+                <div className={"border p-2"}>Nombre de jeton </div>
+                <div className={"border p-2"}>{currentIntensityTokenAmount} / {rkThreshold}</div>
             </section>
             <section>
                 <GaugeChart
@@ -45,13 +51,16 @@ function App() {
                     arcWidth={0.3}
                     percent={percent}
                     needleColor={"#000000"}
-                    hideText={true}
                     animDelay={0}
+                    formatTextValue={() => trackingIntensity.toString()}
                 />
             </section>
-            <section className={"flex justify-center gap-3"}>
-                <button onClick={removeToken}>-</button>
-                <button onClick={addToken}>+</button>
+            <section className={"max-w-sm flex flex-col items-center-center gap-3"}>
+                <div className={"flex flex-row justify-center gap-3"}>
+                    <button className={"flex-1"} onClick={removeToken}>-</button>
+                    <button className={"flex-1"} onClick={addToken}>+</button>
+                </div>
+                <button onClick={resetToken}>Reset</button>
             </section>
         </main>
     )
